a17399e49872fd53d970af4bf1f70fafdbce431e
Reza Hossaini <reza.hossaini@edu.fh-kaernten.ac.at>
2022-06-30 13:08:06 +0200

    push repo

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// Hamburger Icon onClick
+
+let hm = document.querySelector(".hamburger-icon")
+hm.addEventListener("click", () => {
+  hm.classList.toggle("open")
+  console.log("clicked")
+});
+
+hm.addEventListener("keypress", (event)=> {
+  if (event.keyCode === 13) { // key code of the keybord key
+    event.preventDefault();
+    hm.classList.toggle("open")
+  }
+});
